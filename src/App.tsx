982988bd6b2b85bf7363f1d437d96b9115856c6d
ash4rk <cool.shark.98@mail.ru>
2023-02-16 02:39:47 +0300

    FIX: Resource reference for PostList

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 
 const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="posts" list={ListGuesser} />
+    <Resource name="posts" list={PostList} />
     <Resource name="users" list={UserList} recordRepresentation="name" />
   </Admin>
 );
