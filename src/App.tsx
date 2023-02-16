ec43299ebd83bbe18fe892950be32dce11ed2a61
ash4rk <cool.shark.98@mail.ru>
2023-02-16 03:49:08 +0300

    FEAT: Add LoginScreen

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,16 @@ import UserIcon from "@mui/icons-material/Group";
 import { UserList } from "./users";
 import { PostList, PostEdit, PostCreate } from "./posts";
 import { Dashboard } from "./Dashboard";
+import { authProvider } from "./authProvider";
 
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 
 const App = () => (
-  <Admin dataProvider={dataProvider} dashboard={Dashboard}>
+  <Admin
+    authProvider={authProvider}
+    dataProvider={dataProvider}
+    dashboard={Dashboard}
+  >
     <Resource
       name="posts"
       list={PostList}
